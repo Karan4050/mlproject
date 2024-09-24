400f6bc475aa961eff996640ae6881232e0488ab
Karan4050 <karanparmar9733@gmail.com>
2024-09-24 14:01:26 +0530

    Describe whar changes made in logger.py

--- src/logger.py
+++ src/logger.py
@@ -2,30 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-# Generate the log file name with the current date and time
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# Create the directory for the logs
-logs_dir = os.path.join(os.getcwd(), "logs")
-os.makedirs(logs_dir, exist_ok=True)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-# Define the full path to the log file
-LOG_FILE_path = os.path.join(logs_dir, LOG_FILE)
-
-# Configure the logging
 logging.basicConfig(
-    filename=LOG_FILE_path,
+    filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+
+
 )
-
-# Correct the main block
-if __name__ == "__main__":
-    logging.info("Logging has started")
-
-    # Add a print statement to verify if this block is running
-    print("Logging has started and should be written to", LOG_FILE_path)
-
-    # Manually flush the logging handlers
-    for handler in logging.getLogger().handlers:
-        handler.flush()
